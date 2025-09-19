543c69d1ae26fe1b911d37104887e77b12974ef8
欣欣子 <xinxinzi@xinxinzideMacBook-Pro.local>
2025-09-20 01:41:53 +0800

    chore(analytics): move Plausible script to index.html head for verification; remove Helmet injection

--- src/Root.jsx
+++ src/Root.jsx
@@ -9,8 +9,6 @@ const AppContent = () => {
       <Helmet>
         <title>大海团队官网</title>
         <meta name="description" content="一个由大海团队创建的应用。" />
-        <script defer data-domain="dhtd.vercel.app" src="https://plausible.io/js/script.file-downloads.hash.outbound-links.pageview-props.tagged-events.js"></script>
-        <script>{`window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }`}</script>
       </Helmet>
       <Outlet />
     </>
